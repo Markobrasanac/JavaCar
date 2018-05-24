531c8df7606d26e42620da9321d0838e00c6d895
Marko <markobra123@gmail.com>
2018-05-24 20:26:49 +0200

    added methoed for travel

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,6 @@
 
 public class Car {
-    
+
     int maxSpeed = 120;
     int minSpeed = 10;
     double weight = 1000;
@@ -9,15 +9,24 @@ public class Car {
     char condition = 'A';
     double currentPrice = 3000;
     int mileagePassed = 50000;
-    
-    public Car(){
+
+    String model;
+    int maxFuel;
+    int currentFuel;
+    int Consumption;
+    int distance;
+
+    public Car() {
+
+        this.model = "default";
+        this.currentFuel = 0;
+        this.maxFuel = 100;
+
     }
-    
-    public Car(int maxSpeed, int minSpeed, double weight, boolean isTheCarOn, String license, char condition, double currentPrice  
-    
-        , int mileagePassed)
-    { 
-        
+
+    public Car(int maxSpeed, int minSpeed, double weight, boolean isTheCarOn, String license, char condition, double currentPrice,
+            int mileagePassed) {
+
         this.maxSpeed = maxSpeed;
         this.minSpeed = minSpeed;
         this.weight = weight;
@@ -26,12 +35,61 @@ public class Car {
         this.condition = condition;
         this.currentPrice = currentPrice;
         this.mileagePassed = mileagePassed;
-    
-        
-        
-        
+
     }
-    
-    
-    
+
+    public void printMainAttributes() {
+        System.out.println("Model: " + this.model);
+        System.out.println("Kapacitet rezervoara: " + this.maxFuel);
+
+        System.out.println("Trenutno gorivo: " + this.currentFuel);
+        System.out.println("Predjena kilometraza: " + this.mileagePassed);
+        System.out.println();
+
+    }
+
+    public void printAttributes() {
+        System.out.println("Maksimalna brzina je: " + this.maxSpeed);
+        System.out.println("Minimalna brzina je: " + this.minSpeed);
+        System.out.println("Registracioni broj je: " + this.license);
+        System.out.println("Tezina je: " + this.weight);
+        System.out.println("Trenutna cena: " + this.currentPrice);
+        System.out.println("Predjena kilometraza: " + this.mileagePassed);
+
+    }
+
+    public void changeModel(String customModel) {
+        this.model = customModel;
+
+    }
+
+    public void ChangeMaxFuel(int customMaxFuel) {
+
+        this.maxFuel = customMaxFuel;
+
+    }
+
+    public void fuelUp() {
+
+        this.currentFuel = this.maxFuel;
+
+    }
+
+    public void changeConsumption(int customConsumption) {
+
+        this.Consumption = customConsumption;
+    }
+
+    public void travel(int distance) {
+
+        if (this.currentFuel > distance * this.Consumption) {
+            this.mileagePassed = this.mileagePassed + distance;
+            this.currentFuel = this.currentFuel - distance * this.Consumption;
+            System.out.println("putovali ste:" + distance);
+        } else {
+            System.out.println("Nema dovoljno goriva");
+
+        }
+
+    }
 }
